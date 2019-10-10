95444d998fe0c39b067ed83f0baf71aeb64ff993
Kyle Fowler <kylespell101@gmail.com>
2019-10-09 19:01:35 -0700

    Added Rarity

---
+++ Assets/Scripts/PokemonData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonData : MonoBehaviour
+{
+    //Rarities: 
+    //Common - 4
+    //Uncommon - 3
+    //Rare - 2
+    //Ultra Rare - 1
+    public int rarity;
+    //Shiny or not, Shiny is always Ultra Rare
+    public bool shiny;
+    //Pokedex Number of the Pokemon
+    public int pokeNum;
+    //Is it seen?
+    public bool seen;
+    //Is it captured
+    public bool captured;
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public List<GameObject> objectsToSpawn;
+    public List<GameObject> objectPool;
+    public List<GameObject> objectToSpawn;
 
     [ReadOnlyField]
     public float timer = 0;
+
     public int timeBetween;
+
+    private void Awake()
+    {
+        int i = 0;
+        foreach (GameObject obj in objectPool)
+        {
+            
+            for (int j = 0; j < objectPool[i].GetComponent<PokemonData>().rarity; j++)
+            {
+                objectToSpawn.Add(objectPool[i]);
+            }
+            i++;
+        }
+    }
 
     private void Update()
     {
         timer += Time.deltaTime;
         if (timer >= timeBetween)
         {
-            int rand = Random.Range(0, objectsToSpawn.Count);
-            Instantiate(objectsToSpawn[rand], gameObject.transform);
+            int rand = Random.Range(0, objectToSpawn.Count);
+            GameObject spawned = Instantiate(objectToSpawn[rand], gameObject.transform);
+            spawned.GetComponent<PokemonData>().seen = true;
+            Debug.Log(spawned.name);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                spawned.GetComponent<PokemonData>().captured = true;
+            }
             timer = 0;
         }
+
     }
 }
