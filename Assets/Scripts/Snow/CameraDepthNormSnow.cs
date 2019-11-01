b326d24b9788a92fd4e19105d0d775aa7200d1b7
LegendaryyDoc <nateknebusch@gmail.com>
2019-11-01 12:35:19 -0700

    removing my scene becauses doing weird things

---
+++ Assets/Scripts/Snow/CameraDepthNormSnow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraDepthNormSnow : MonoBehaviour
+{
+    public Texture2D snowTexture;
+    public Color snowColor = Color.white;
+    public float snowTextureScale = 0.1f;
+
+    [Range(0, 1)]
+    public float botThreshold = 0f;
+    [Range(0, 1)]
+    public float topThreshold = 1f;
+
+    private Material _material;
+
+    void OnEnabled()
+    {
+        _material = new Material(Shader.Find("Unlit/SnowOverlay"));
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.DepthNormals;
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        _material.SetMatrix("_CamToWorld", GetComponent<Camera>().cameraToWorldMatrix);
+        _material.SetColor("_SnowColor", snowColor);
+        _material.SetFloat("_BottomThreshold", botThreshold);
+        _material.SetFloat("_TopThreshold", topThreshold);
+        _material.SetTexture("_SnowTex", snowTexture);
+        _material.SetFloat("_SnowTexScale", snowTextureScale);
+
+        Graphics.Blit(source, destination, _material);
+    }
+}
