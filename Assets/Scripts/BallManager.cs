ef36cb32a0c6b62f6f8fd7aa0bb49aa66cfe677c
Sapphire Fowler <kylespell101@gmail.com>
2019-11-21 17:39:18 -0800

    Started item system

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallManager : MonoBehaviour
+{
+    public List<Sprite> possibleItems;
+    private Sprite itemToSpawn;
+
+    public Camera mainCamera;
+
+    public Canvas canvas;
+    public Image image;
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject == gameObject)
+            {
+                ChooseItem();
+            }
+        }
+    }
+
+    public void ChooseItem()
+    {
+        int rand = Random.Range(0, possibleItems.Count);
+        itemToSpawn = possibleItems[rand];
+
+        canvas.enabled = true;
+        image.GetComponent<Image>().sprite = itemToSpawn;
+    }
+}
